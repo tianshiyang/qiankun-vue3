42e1f9593a2abe90bb3505e0e6e0e52ded580346
tianshiyang <1685821150@qq.com>
2024-06-05 09:47:30 +0800

    feat: 环境适配

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 const { name } = require('./package.json');
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? "/vue3-app/" : "/",
   transpileDependencies: true,
   devServer: {
-    port: 5000,
+    port: 5005,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-    }
+      "Access-Control-Allow-Origin": "*",
+    },
   },
-  configureWebpack: config => {
+  configureWebpack: (config) => {
     config.output.library = `${name}-[name]`
     config.output.libraryTarget = "umd"
     config.output.chunkLoadingGlobal = `webpackJsonp_${name}`
